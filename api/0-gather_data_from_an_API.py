a16fbd90ee86c237a99699d3a60124797482cc18
seeyou7 <5764@holbertonstudents.com>
2023-12-20 11:04:11 +0100

    my original code after testing checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" using rest api to get info """
-
+'''
+Python script that returns information using REST API
+'''
 import requests
 import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    user_url = (f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     employee_data = requests.get(user_url).json()
+    
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    employee_tasks = requests.get(todos_url).json()
 
-    todo_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    )
-    employee_tasks = requests.get(todo_url).json()
-    completed_tasks = [
-        task for task in employee_tasks if task.get("completed")
-    ]
+    completed_tasks = [task for task in employee_tasks if task.get("completed")]
 
     print(
         f"Employee {employee_data['name']} is done with "
